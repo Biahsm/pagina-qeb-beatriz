d3c88edff1bdd66b1456e910c0c39ed18620fc35
Biahsm <souza.matos.beatriz@escola.pr.gov.br>
2025-05-28 00:26:12 +0000

    começando javascript

---
+++ perguntas.js
@@ -0,0 +1,17 @@
+criaCartao(
+    'Geografia',
+    'Quantos estados tem o Brasil?',
+    '26, mais o Distrito Federal'
+)
+
+criaCartao(
+    'Ciências',
+    'Qual é o planeta mais próximo do Sol?',
+    'Mercúrio'
+)
+
+criaCartao(
+    'Artes',
+    'Quem escreveu Dom Quixote?',
+    'Miguel de Cervantes'
+)
